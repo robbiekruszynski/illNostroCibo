2ec3daa58cb00a084c3daa5d24f5f19687f51cd0
robbie <robbiekruszynski@gmail.com>
2020-05-17 16:31:05 -0700

    add grid, navbar ideas

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import Cards from "./Cards";
 import NavBar from "./Nav";
+import Grid from "./Grid";
 function App() {
   return (
     <div className="App">
-      <div className="Navbar">
+      <div className="navbar">
         <NavBar />
       </div>
-      <Cards />
-      <Cards />
+      <div className="centered"></div>
+      <div className="cards">
+        {/* <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards /> */}
+        <div></div>
+        <Grid />
+      </div>
     </div>
   );
 }
